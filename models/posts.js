b618d1d381daac500a11fad0588ad355ac495fff
unknown <richwbates@gmail.com>
2017-11-04 14:35:17 -0400

    added post schema

---
+++ models/posts.js
@@ -0,0 +1,31 @@
+module.exports = function(sequelize, DataTypes) {
+	const posts= sequelize.define('posts', {
+		uuid: {
+			type 	: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV1,
+    		primaryKey: true,
+			allowNull: false,
+		},
+		user_uuid: {
+			type: DataTypes.UUID,
+			allowNull: false
+		},
+		thread_uuid: {
+			type: DataTypes.UUID,
+			allowNull: false
+		},
+		text: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+			validate: {
+				len: {
+					args: [15],
+					msg: 'Post must be a minimum of 15 characters'
+				}
+			}
+		}
+	});
+
+	thread.sync();
+	return thread;
+}
