856576cf4402c97f119021765502246453d02768
KBerry4492 <kmb23@comcast.net>
2017-11-04 13:29:11 -0400

    RouteFiddle

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../models");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-  //(fileCalled, {data: passed});
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+
+// index route loads view.html
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+
+// cms route loads cms.html
+app.get("/forum", function(req, res) {
+	res.sendFile(path.join(__dirname, "../public/forum.html"));
+});
+
+// blog route loads blog.html
+app.get("/thread", function(req, res) {
+	res.sendFile(path.join(__dirname, "../public/thread.html"));
+});
+
+app.get("/post", function(req, res) {
+	res.sendFile(path.join(__dirname, "../public/post.html"));
+});
+
+// authors route loads author-manager.html
+app.get("/profile", function(req, res) {
+	res.sendFile(path.join(__dirname, "../public/profile.html"));
+});
+
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+//   //(fileCalled, {data: passed});
+// });
 
 module.exports = router;
--- routes/thread.js
+++ routes/thread.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../models");
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+app.get("/api/thread", function(req, res) {
+	db.Thread.findAll({
+		include: [db.Post],
+	}).then(function(dbThread) {
+		res.json(dbThread);
+	});
+});
+
+app.get("/api/thread/:id", function(req, res) {
+	db.Thread.findOne({
+		include: [db.Post],
+		where: {
+			id: req.params.id
+		}
+	}).then(function(dbThread) {
+		res.json(dbThread);
+	});
+});
+
+app.post("/api/thread", function(req, res) {
+	db.Thread.create(req.body).then(function(dbThread) {
+		res.json(dbThread);
+	});
+});
+
+app.delete("/api/thread/:id", function(req, res) {
+	db.Thread.destroy({
+		where: {
+			id: req.params.id
+		}
+	}).then(function(dbThread) {
+		res.json(dbThread);
+	});
 });
 
 module.exports = router;
