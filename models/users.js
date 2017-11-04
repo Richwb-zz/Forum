574746c34d7bdc530c242b7203c9af66defc95fb
unknown <richwbates@gmail.com>
2017-11-04 13:25:31 -0400

    added users table schema

---
+++ models/users.js
@@ -0,0 +1,63 @@
+module.exports = function(sequelize, DataTypes) {
+	const forum = sequelize.define('forum', {
+		uuid: {
+			type 	: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV1,
+    		primaryKey: true,
+			allowNull: false,
+		},
+		username: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: {
+					args: [1, 20],
+					msg: 'Username length must be between 8 and 25 characters'
+				}
+			}
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: {
+					args: [8, 25],
+					msg: 'Password length must be between 8 and 25 characters'
+				}
+			}
+		},
+		Firstname: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: {
+					args: [1, 25],
+					msg: 'first Name length must be between 1 and 25 characters'
+				}
+			}
+		},
+		Lastname: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: {
+					args: [1, 25],
+					msg: 'Last Name length must be between 1 and 25 characters'
+				}
+			}
+		},
+		Email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: {
+					args: [1, 50],
+					msg: 'Last Name length must be between 1 and 25 characters'
+				}
+			}
+		},
+	});
+
+	forum.sync();
+	return forum;
+}
