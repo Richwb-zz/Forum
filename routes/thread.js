760e71b4e66541270633919cba1770cd9c753688
KBerry4492 <kmb23@comcast.net>
2017-11-04 12:44:15 -0400

    empty routes

--- routes/thread.js
+++ routes/thread.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-  //(fileCalled, {data: passed});
+  res.send('respond with a resource');
 });
 
 module.exports = router;
