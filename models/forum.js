db432e56895dec7d41899e7a9e7a38dd53dcb2ae
unknown <richwbates@gmail.com>
2017-11-04 13:34:09 -0400

    added forum table schema

---
+++ models/forum.js
@@ -0,0 +1,33 @@
+module.exports = function(sequelize, DataTypes) {
+	const forum = sequelize.define('forum', {
+		uuid: {
+			type 	: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV1,
+    		primaryKey: true,
+			allowNull: false,
+		},
+		forum_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: {
+					args: [1, 50],
+					msg: 'Forum Name length must be between 1 and 50 characters'
+				}
+			}
+		},
+		description: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: {
+					args: [10, 255],
+					msg: 'Password length must be between 10 and 255 characters'
+				}
+			}
+		},
+	});
+
+	forum.sync();
+	return forum;
+}
