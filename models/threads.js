2672b797c07ae2aff6cffe27023a164f87ddaf04
unknown <richwbates@gmail.com>
2017-11-07 20:49:49 -0500

    removed thread_uuid, added associations

--- models/threads.js
+++ models/threads.js
@@ -1,11 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
 	const thread = sequelize.define('thread', {
-		uuid: {
-			type 	: DataTypes.UUID,
-			defaultValue: DataTypes.UUIDV1,
-    		primaryKey: true,
-			allowNull: false,
-		},
 		thread_name: {
 			type: DataTypes.STRING(100),
 			allowNull: false,
@@ -15,11 +9,6 @@ module.exports = function(sequelize, DataTypes) {
 					msg: 'Thread Name length must be between 10 and 100 characters'
 				}
 			}
-		},
-		forum_uuid: {
-			type: DataTypes.UUID,
-			defaultValue: DataTypes.UUIDV1,
-			allowNull: false
 		},
 		created_By: {
 			type: DataTypes.UUID,
@@ -35,6 +24,21 @@ module.exports = function(sequelize, DataTypes) {
 			type: DataTypes.DATE,
 			allowNull: false
 		},
+	}
+	{
+		underscored: true,
+		freezeTableName: true,
+		classMethods: {
+			associate: function(models) {
+				thread.hasMany(models.posts)
+				thread.belongsTo(models.forum,{
+					foreignKey: {
+						foreignKey: 'forum_id'
+						allowNull: false
+					}
+				});
+			}
+		}
 	});
 
 	thread.sync();
