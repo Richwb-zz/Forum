0e4ed1444046e8fc8fddf5fe390a5f46fe4a07a8
unknown <richwbates@gmail.com>
2017-11-10 09:59:39 -0500

    removed test required

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var queries = require('./test.js');
-
 
 // /* GET users listing. */
 // router.get('/', function(req, res, next) {
