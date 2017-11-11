10ee85ad46917531f7e5bdfc2ab999950e8fa8d9
Michael Devaney <Mike@Mike.local>
2017-11-11 11:13:10 -0500

    queries update

--- queries.js
+++ queries.js
@@ -1,7 +1,6 @@
 var models = require("models/index.js");
 
-
-function signInValidation(user){
+module.exports.signInValidation = function(user){
 	models.users.findAll({
 	[Op.or]: [
     {
@@ -17,3 +16,38 @@ function signInValidation(user){
 	})
 };
 
+module.exports.getUser = function(num) {
+  return new Promise((resolve, reject) => {
+    var userId = parseInt(num);
+    // Make query
+    if (user_id) {
+      models.users.findOne({
+        include: [{ profile: models.profiles }],
+        where: { uuid: user_id },
+      }).then((results) => {
+        if (!results) {
+          resolve({}); // if no user, return empty object not NULL
+        } else {
+          resolve(results);
+        }
+      }); 
+    } else {
+      models.users.findAll({}).then((results) => {
+        if (!results) {
+          resolve({}); // if no user, return empty object not NULL
+        } else {
+          resolve(results);
+        }
+      })
+    }
+  }); // end of Promise
+};
+
+module.exports.getForum = function(){
+  models.forum.findAll({
+    
+  });
+    .then(forum => {
+
+    })
+};
